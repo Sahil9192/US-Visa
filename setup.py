506b48285984db26b4e442df947268736038a938
Sahil9192 <sahilypawar9192@gmail.com>
2025-03-29 22:06:44 +0530

    Database created and checked

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sahil",
+    author_email="sahilypawar9192@gmail.com",
+    packages=find_packages()
+)
